993d019e3508ba66eb0d8ab6f33e63a7d33e3797
Sofia Dandjee <sofia.dandjee@epfl.ch>
2019-09-24 08:53:02 +0200

    ultimate question

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,10 @@
 public class helloworld {
   public static void main(String[] args) {
     System.out.println("Hello Sweng!");
+    System.out.println("The ultimate question of Life, The Universe, and Everything is: " + computeUltimateQuestion());
   }
+}
+
+public static String computeUltimateQuestion() {
+	return "still searching...";
 }
